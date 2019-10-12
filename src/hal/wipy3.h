055a0dbde6347c599c34ae13752561d40f02ec15
Wolfgang Nagele <mail@wnagele.com>
2019-10-12 12:59:05 +0200

    Use GPIO_NUM header definition

--- src/hal/wipy3.h
+++ src/hal/wipy3.h
@@ -10,8 +10,8 @@
 #define HAS_LED NOT_A_PIN
 #define HAS_RGB_LED SmartLed rgb_led(LED_WS2812, 1, GPIO_NUM_0) // WS2812B RGB LED on GPIO0 (P2)
 
-#define HAS_ANTENNA_SWITCH (21)       // pin for switching wifi antenna
-#define WIFI_ANTENNA 1                // 0 = internal, 1 = external
+#define HAS_ANTENNA_SWITCH GPIO_NUM_21 // pin for switching wifi antenna
+#define WIFI_ANTENNA 1                 // 0 = internal, 1 = external
 
 #define HAS_SPI 1
 #define SPI_CS   GPIO_NUM_15
